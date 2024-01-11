8016f102e192fbec5c959b44c6db0f784d8e55cd
Abdulsalamdev <abdulsalamakinyoola@gmail.com>
2024-01-11 09:29:44 +0100

    part1 done

--- components/fullstackopen/part1/section2.tsx
+++ components/fullstackopen/part1/section2.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 export const Section2 = () => {
+  const [next, setNext] = useState(0);
   const anecdotes = [
     "If it hurts, do it more often.",
     "Adding manpower to a late software project makes it later!",
@@ -11,22 +12,50 @@ export const Section2 = () => {
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
     "The only way to go fast, is to go well.",
   ];
-  const voting = [];
-  // handle Vote
-  const votes = () => {};
-  // handle next
-  const handleNext = () => {};
+  const voting: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
+  const hasNext = next < anecdotes.length - 1;
+  const vote = voting[next];
 
+  // handle next
+  const handleNext = () => {
+    if (hasNext) {
+      setNext(next + 1);
+    } else {
+      setNext(0);
+    }
+  };
+
+  // handle Vote
+  const handlevote = () => {
+    voting[next] += 1;
+  };
+
+  let anecdote = anecdotes[next];
   return (
     <div className="pt-[50px]">
       <p className="text-[30px] font-bold pb-[20px]">section 2</p>
       <p className="text-[22px] font-bold">anecdote of the day</p>
-      <p className="text-[18px] font-semibold pb-[10px]"></p>
+      <p className="text-[18px] font-semibold pb-[10px]">
+        {anecdote} has {vote}
+      </p>
       <div className="flex gap-[20px] pb-[20px]">
-        <button className="p-[10px] rounded-[5px] feed-btn bg-[whitesmoke]">
+        <button
+          className="p-[10px] rounded-[5px] feed-btn bg-[whitesmoke]"
+          onClick={() => {
+            handlevote();
+            console.log(voting);
+          }}
+        >
           Vote
         </button>
-        <button className="p-[10px] rounded-[5px] feed-btn bg-[whitesmoke]">
+        <button
+          className="p-[10px] rounded-[5px] feed-btn bg-[whitesmoke]"
+          onClick={() => {
+            handleNext();
+            console.log(vote);
+            // console.log(next);
+          }}
+        >
           Next anecdotes
         </button>
       </div>
