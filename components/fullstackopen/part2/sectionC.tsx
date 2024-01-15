d159859d23894f8349569056624acfde0aa5e3b7
Abdulsalamdev <abdulsalamakinyoola@gmail.com>
2024-01-15 09:44:49 +0100

    completed

--- components/fullstackopen/part2/sectionC.tsx
+++ components/fullstackopen/part2/sectionC.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+
+interface notes {
+  name: string;
+  number: string;
+  id: number;
+}
 export const SectionC = () => {
+  const [note, setNote] = useState([]);
+  //getting data from the server
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log(response.data);
+      setNote(response.data);
+    });
+  }, []);
+
   return (
     <div>
       <div className="text-[30px] font-bold ">sectionC</div>
+      <div>
+        {note.map((item: notes) => (
+          <div key={item.id} className="font-medium text-[18px]">
+            id: {item.id} name: {item.name} , details: {item.number}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
