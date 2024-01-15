e3856f188ffb8fa1f61146bf7b193cda83a309c2
Abdulsalamdev <abdulsalamakinyoola@gmail.com>
2024-01-15 08:52:27 +0100

    updating

---
+++ components/fullstackopen/part2/sectionC.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import axios from "axios";
+export const SectionC = () => {
+  return (
+    <div>
+      <div className="text-[30px] font-bold ">sectionC</div>
+    </div>
+  );
+};
--- pages/part2.tsx
+++ pages/part2.tsx
@@ -1,5 +1,6 @@
 import { SeacPart2 } from "@/components/fullstackopen/part2/seactionA";
 import { SeactionBPart2 } from "@/components/fullstackopen/part2/seactionB";
+import { SectionC } from "@/components/fullstackopen/part2/sectionC";
 import React from "react";
 
 const Part2 = () => {
@@ -7,6 +8,7 @@ const Part2 = () => {
     <div className="mt-[30px] ml-[30px]">
       <SeacPart2 />
       <SeactionBPart2 />
+      <SectionC />
     </div>
   );
 };
